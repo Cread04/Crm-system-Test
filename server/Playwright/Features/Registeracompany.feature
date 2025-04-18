6c76907bc640f0dfb1b01489f9cc907119970a7e
Markus Olsson <markus.olsson2006@gmail.com>
2025-04-18 12:25:51 +0200

    nyplaywright

---
+++ server/Playwright/Features/Registeracompany.feature
@@ -0,0 +1,12 @@
+﻿Feature: Registeracomapny
+    
+    Scenario: Register a company
+        Given i am on the homepage
+        When i click on register 
+        Then i should see the register form
+        And i enter "Markus.olsson2006@gmail.com" as the email
+        And i enter "abc123" as the password
+        And i enter "Markus" as the username
+        And i enter "Heavy" as the company name
+        When i click the Skapakonto button
+        
---
+++ server/Playwright/Steps/Registerauser.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.Playwright;
+using TechTalk.SpecFlow;
+
+
+namespace server.Steps;
+
+[Binding]
+public class Registeracompany
+{
+    private readonly IPage _page;
+
+    public Registeracompany(ScenarioContext context)
+    {
+        _page = context["page"] as IPage;
+    }
+
+    [Given(@"i am on the homepage")]
+    public async Task GivenIAmOnTheHomepage()
+    {
+        await _page.GotoAsync("http://localhost:5173");
+    }
+
+    [When(@"i click on register")]
+    public async Task WhenIClickOnRegister()
+    {
+        await _page.ClickAsync("text=Register"); 
+    }
+
+    [Then(@"i should see the register form")]
+    public async Task ThenIShouldSeeTheRegisterForm()
+    {
+        await _page.WaitForSelectorAsync("form"); 
+    }
+
+    [Then(@"i enter ""(.*)"" as the email")]
+    public async Task ThenIEnterAsTheEmail(string email)
+    {
+        await _page.FillAsync("input[name='email']", email);
+    }
+
+    [Then(@"i enter ""(.*)"" as the password")]
+    public async Task ThenIEnterAsThePassword(string password)
+    {
+        await _page.FillAsync("input[name='password']", password);
+    }
+
+    [Then(@"i enter ""(.*)"" as the username")]
+    public async Task ThenIEnterAsTheUsername(string username)
+    {
+        await _page.FillAsync("input[name='username']", username);
+    }
+
+    [Then(@"i enter ""(.*)"" as the company name")]
+    public async Task ThenIEnterAsTheCompanyName(string company)
+    {
+        await _page.FillAsync("input[name='company']", company);
+    }
+
+    [When(@"i click the Skapakonto button")]
+    public async Task WhenIClickTheSkapakontoButton()
+    {
+        await _page.ClickAsync("text=Skapa konto"); // Adjust selector if needed
+    }
+}
